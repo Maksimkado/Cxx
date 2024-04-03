9c97cf8d2e502ea74de31f88c4d28828a4c10bce
Maksimkado <165821626+Maksimkado@users.noreply.github.com>
2024-04-03 05:01:16 +0500

    Update Modul.py

--- Modul.py
+++ Modul.py
@@ -21,5 +21,5 @@ class YourMod(loader.Module):
     async def amscmd(self, message):
         """Используй .ams"""
         logger.debug("We logged something!")
-        await utils.answer(message, requests.get("http://a0938554.xsph.ru/Players.txt"))
+        await utils.answer(message, requests.get("http://a0938554.xsph.ru/Players.txt").text)
      
