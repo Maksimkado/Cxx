0a68327f912ed57b7a3a2171969b0e009593334a
Maksimkado <165821626+Maksimkado@users.noreply.github.com>
2024-04-04 20:46:22 +0500

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,1 +1,24 @@
-w
+import asyncio
+import logging
+
+from .. import loader, utils
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class YourMod(loader.Module):
+    """модули вот"""  # Translateable due to @loader.tds
+    strings = {"cfg_doc": "This is what is said, you can edit me with the configurato",
+               "name": "r",
+               "after_sleep": "We have finished sleeping!"}
+
+    def __init__(self):
+        self.config = loader.ModuleConfig("CONFIG_STRING", "hello", lambda m: self.strings("cfg_doc", m))
+
+    @loader.unrestricted  # Security setting to change who can use the command (defaults to owner | sudo)
+    async def rlcmd(self, message):
+        """Используй .ams"""
+        logger.debug("We logged something!")
+        lol = "rot"
+        await utils.answer(message, {lol})
